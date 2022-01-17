f21811c883ce5cc75fccd29fbb623302e4229c92
liudao <592922218@qq.com>
2022-01-17 10:21:30 +0800

    project init

---
+++ 1.两数之和.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=javascript
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  // nums = [3,2,4], target = 6
+  // 输出：[1,2]
+  let map = new Map();
+  for (let i = 0, len = nums.length; i < len; i++) {
+    if (map.has(target - nums[i])) {
+      return [map.get(target - nums[i]), i];
+    } else {
+      map.set(nums[i], i);
+    }
+  }
+  return [];
+};
+// @lc code=end
